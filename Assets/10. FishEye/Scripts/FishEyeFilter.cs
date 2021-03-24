c6e74903775abaaee9920cabdd3ed2cc9e2a741d
Karamelka322 <aafanya@bk.ru>
2021-03-24 08:29:01 +0700

    First commit

---
+++ Assets/10. FishEye/Scripts/FishEyeFilter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FishEyeFilter : SimpleFilter
+{
+    [SerializeField]
+    private float _pow;
+
+    protected override void OnUpdate()
+    {
+        _mat.SetFloat("_BarrelPower", _pow);
+    }
+}
---
+++ Assets/9. Warping/Scripts/WarpingFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpingFilter : SimpleFilter
+{
+    [Range(0, 10)]
+    [SerializeField]
+    private float _amplitude = 5;    
+    
+    [Range(0, 50)]
+    [SerializeField]
+    private float _speed = 15;
+
+    protected override void OnUpdate()
+    {
+        _mat.SetFloat("_Amplityde", _amplitude);
+        _mat.SetFloat("_Speed", _speed);
+    }
+}
